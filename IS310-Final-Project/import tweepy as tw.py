28e23936918a2248d3f53ec28da810bcd713ae36
Rithvik Mandumula <rithvik.mandumula@hotmail.com>
2024-05-11 00:38:14 -0500

    commit

--- IS310-Final-Project/import tweepy as tw.py
+++ IS310-Final-Project/import tweepy as tw.py
@@ -2,30 +2,20 @@ import numpy as np  # Linear algebra
 import pandas as pd  # Data processing, CSV file I/O (e.g. pd.read_csv)
 import tweepy as tw  # To extract Twitter data
 from tqdm import tqdm  # For progress bar
-
 # Twitter API credentials
 consumer_api_key = 'your_consumer_api_key'
 consumer_api_secret = 'your_consumer_api_secret'
-
 # OAuth authentication
 auth = tw.OAuthHandler(consumer_api_key, consumer_api_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
-
 # Define the search query
-search_words = "#ipl2023 -filter:retweets"
-date_since = "2023-04-25"
-date_until = "2023-06-01"
-
+search_words = "#ipl2021 -filter:retweets"
+date_since = ""
+date_until = ""
 # Collect tweets
-tweets = tw.Cursor(api.search_tweets,
-                   q=search_words,
-                   lang="en",
-                   since=date_since,
-                   until=date_until).items(7500)
-
+tweets = tw.Cursor(api.search_tweets, q=search_words, lang="en", since=date_since,until=date_until).items(7500)
 # Prepare to collect tweets
 tweets_list = []
-
 # Loop through each tweet
 for tweet in tqdm(tweets):
     hashtags = [hashtag['text'] for hashtag in tweet.entities['hashtags']]
@@ -44,10 +34,8 @@ for tweet in tqdm(tweets):
         'source': tweet.source,
         'is_retweet': tweet.retweeted
     })
-
 # Create DataFrame
 tweets_df = pd.DataFrame(tweets_list)
-
 # Save the DataFrame to a CSV file
-tweets_df.to_csv('ipl2023.csv', index=False)
+tweets_df.to_csv('ipl_2021_tweets.csv', index=False)
 print(f"New tweets retrieved: {len(tweets_df)}")
